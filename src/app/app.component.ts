dc2e6f0847aee650c4e0e991a988bcf68636ce79
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2021-11-21 16:00:19 +0900

    create comment model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { Comment } from './models/comment.model';
+
+const mockComments: Comment[] = [
+  { name: '大島 渚', message: '皆様お変わりなくお過ごしでしょうか'},
+  { name: '但馬 香苗', message: '天候不順のみぎり、お元気でご活躍のことと存じます'},
+  { name: '板取 俊介', message: 'すっかりご無沙汰いたしまして、申し訳ありません'},
+  { name: '美和 光', message: '平素は何かとお心にかけて下さいまして有難うございます'},
+]
 
 @Component({
   selector: 'ca-root',
@@ -6,5 +14,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'chat-app';
+
+  comments: Comment[]
+
+  constructor() {
+    this.comments = mockComments
+  }
 }
